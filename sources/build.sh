a97e0a49ca33cefc7409fcb6508fe6fad50a7e35
Rosalie Wagner <rose.wagner@free.fr>
2020-09-22 12:29:55 +0200

    Added designspace and build script

---
+++ sources/build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e
+source ../env/bin/activate
+
+# Mute these lines if you don't want to regenerate UFOs
+echo ".
+MAKE UFO
+."
+rm -rf *.ufo
+glyphs2ufo Ballet.glyphs
+rm Ballet.designspace
+
+##########################################
+
+echo ".
+GENERATING VARIABLE
+."
+rm -rf ../fonts/variable
+mkdir -p ../fonts/variable
+VF_PATH=..fonts/variable/Ballet[opsz].ttf
+fontmake -m Ballet-variable.designspace -o variable --output-path $VF_PATH
+
+##########################################
+
+echo ".
+POST-PROCESSING VF
+."
+vfs=$(ls $VF_PATH)
+for font in $vfs
+do
+	gftools fix-dsig --autofix $font
+	gftools fix-nonhinting $font $font.fix
+	mv $font.fix $font
+	gftools fix-unwanted-tables --tables MVAR $font
+done
+rm ../fonts/variable/*gasp*
+
+python gen_stat.py $VF_PATH
+
+rm -rf instance_ufo/
+
+echo ".
+COMPLETE!
+."
