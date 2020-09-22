63aecb9fedabaa1b47c22b9c1ad1821a0acfcf29
Rosalie Wagner <rose.wagner@free.fr>
2020-09-22 12:29:55 +0200

    Added designspace and build script

---
+++ sources/gen_stat.py
@@ -0,0 +1,48 @@
+from fontTools.otlLib.builder import buildStatTable, _addName
+from fontTools.ttLib import TTFont
+import sys
+
+
+AXES = [
+    dict(
+        tag="opsz",
+        name="Optical size",
+        ordering=0,
+        values=[
+            dict(nominalValue=16, rangeMinValue=16, rangeMaxValue=16, name="16pt"),
+            dict(nominalValue=72, rangeMinValue=72, rangeMaxValue=72, name="72pt"),
+        ],
+    ),
+#    dict(
+#        tag="wght",
+#        name="Weight",
+#        ordering=1,
+#        values=[
+#            dict(nominalValue=400, rangeMinValue=400, rangeMaxValue=400,name="Regular", flags=0x2),
+#        ],
+#    ),
+]
+
+def update_fvar(ttfont):
+    fvar = ttfont['fvar']
+    nametable = ttfont['name']
+    family_name = nametable.getName(16, 3, 1, 1033) or nametable.getName(1, 3, 1, 1033)
+    family_name = family_name.toUnicode().replace(" ", "")
+    nametable.setName(family_name, 25, 3, 1, 1033)
+    for instance in fvar.instances:
+        instance_style = nametable.getName(instance.subfamilyNameID, 3, 1, 1033).toUnicode()
+        ps_name = f"{family_name}-{instance_style.replace(' ', '')}"
+        instance.postscriptNameID = _addName(nametable, ps_name, 256)
+
+
+def main():
+    filepath = sys.argv[1]
+    tt = TTFont(filepath)
+    buildStatTable(tt, AXES)
+    update_fvar(tt)
+    tt.save(filepath)
+    print(f"Added STAT table to {filepath}")
+
+
+if __name__ == "__main__":
+    main()
